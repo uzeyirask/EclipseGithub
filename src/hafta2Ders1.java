f5483fb0200ad2d2d6ea222d696b5f75021b6653
Üzeyir <zeyir@DESKTOP-157U7A>
2022-10-04 22:01:56 +0300

    test commit

---
+++ src/hafta2Ders1.java
@@ -0,0 +1,12 @@
+
+public class hafta2Ders1 {
+	public static void main(String[] args){
+		double a=5.5;
+		System.out.println(a);
+		if(a==5.5 || a==8) {
+			System.out.println("yazildi");
+		}else {
+			System.out.println("elseye girdi");
+		}
+	}
+}
